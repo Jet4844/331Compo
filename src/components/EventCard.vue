9034652400274b7911c50d7828333b737eb16375
Alinjx <chanchanok.2544@gmail.com>
2022-07-22 02:27:50 +0700

    4.7-4.11

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,32 +1,24 @@
 <template>
   <div class="event-card">
     <span>@{{ event.time }} on {{ event.date }}</span>
-    <h4>{{ event.titie }}</h4>
+    <h4>{{ event.title }}</h4>
   </div>
 </template>
 
 <script>
 export default {
   name: 'EventCard',
-  data() {
-    return {
-      event: {
-        id: 5928101,
-        category: 'animal welfare',
-        titie: 'Cat Adoption Day',
-        description: 'Find your new feline friend at this event.',
-        location: 'Meow Town',
-        date: 'January 28, 2022',
-        time: '12:00',
-        petAllowed: true,
-        oeganizer: 'Kat Latdee'
-      }
+  props: {
+    event: {
+      type: Object,
+      required: true
     }
   }
 }
 </script>
+
 <style scoped>
-event-card {
+.event-card {
   padding: 20px;
   width: 250px;
   cursor: pointer;
@@ -35,6 +27,6 @@ event-card {
 }
 .event-card:hover {
   transform: scale(1.01);
-  box-shadow: 0 3px 12 px 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
+  <div class="events">
     <!-- <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
@@ -14,6 +14,30 @@ export default {
   name: 'HomeView',
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 5928101,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline friend at this event.',
+          location: 'Meow Town',
+          date: 'January 22, 2022',
+          time: '12:00',
+          petsAllowed: false,
+          organizer: 'Carey Wales'
+        }
+      ]
+    }
   }
 }
 </script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
